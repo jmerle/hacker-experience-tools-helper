a8ac9e3194912253250dde8601710a008c22aae3
Jasper van Merle <jaspervmerle@gmail.com>
2016-12-10 16:45:57 +0100

    Create HEToolsHelper.meta.js

---
+++ HEToolsHelper.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         HETools Helper
+// @namespace    HEToolsHelper
+// @version      1.3.0
+// @description  A small script that enables you to sync your Hacker Experience HDB with HETools
+// @author       Jasper van Merle
+// @match        https://legacy.hackerexperience.com/*
+// @match        https://en.hackerexperience.com/*
+// @match        https://br.hackerexperience.com/*
+// @grant        none
+// ==/UserScript==
